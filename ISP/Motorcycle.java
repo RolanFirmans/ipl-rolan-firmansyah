1242cee47b2b8952ffc4238b9c2302110009c8e0
Rolan Firmansyah <rolanfirmansyah1@gmail.com>
2023-10-30 15:05:59 +0700

    isp update

--- ISP/Motorcycle.java
+++ ISP/Motorcycle.java
@@ -1,5 +1,8 @@
-class Motorcycle : VehicleInterface {
+class Motorcycle:VehicleInterface,StopInterface
+{
 override fun drive() {}
+
 override fun stop() {}
+
 override fun refuel() {}
 }
---
+++ ISP/StopInterface.java
@@ -0,0 +1,3 @@
+interface StopInterface {
+    void StopInt();
+}
